206d1fbab61b7fd73ff8224c40ae5bd709047ce1
lahcen404 <lahcen.maskour2003@gmail.com>
2025-09-02 16:46:14 +0200

    add challenge 1 condition2

--- conditions 2/challenge1.c
+++ conditions 2/challenge1.c
@@ -2,6 +2,25 @@
 
 int main() {
    
-    double revAnnuel;
-    
+    float revAnnuel;
+    int scoreCredit=0;
+    int dureePret=0;
+
+    printf("entre Revenu Annuel :\n" );
+    scanf("%f",&revAnnuel);
+
+    printf("entre Score Credit :\n" );
+    scanf("%d",&scoreCredit);
+
+    printf("entre Duree pret :\n" );
+    scanf("%d",&dureePret);
+
+    if(revAnnuel >= 30000 && scoreCredit >= 700 && dureePret <= 10){
+        printf("Eligible !!");
+    }else if (revAnnuel >= 30000 && scoreCredit >= 650 && dureePret <= 15){
+        printf("Eligible avec conditions");
+    }else{
+        printf("non eligible !!");
+    }
+
 }
