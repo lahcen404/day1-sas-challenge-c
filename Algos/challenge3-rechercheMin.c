7f33d1d9597c697c77c97dd958793ce15fe96efa
lahcen404 <lahcen.maskour2003@gmail.com>
2025-09-08 18:05:41 +0200

    challenge 3 - Recherche add display index

--- Algos/challenge3-rechercheMin.c
+++ Algos/challenge3-rechercheMin.c
@@ -4,15 +4,18 @@ int main(){
 
         int tab[10]={5,4,2,7,9,3,4,1,7,9};
         int min=tab[0];
+        int index=0;
 
         for (int i = 1; i < 10; i++)
         {
             if (min > tab[i])
             {
                 min=tab[i];
+                index = i;
             }
             
         }
         
-        printf("Min is : %d",min);
+        printf("Min is : %d , and index is %d",min,index);
+
 }
